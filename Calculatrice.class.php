6bd6bb66e80b7582806ff26bca42fbb3cdb233d6
Deudjui Wilfried <wilfried.deudjui@gmail.com>
2014-09-30 12:24:56 +0100

    Ajout de la classe Calculatrice permettant de faire addition, soustraction, multiplication et division

---
+++ Calculatrice.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class Calculatrice {
+
+	public static function additionner($a, $b)
+	{
+		return ($a+$b);
+	}
+	
+	public static function soustraire($a, $b)
+	{
+		return ($a-$b);
+	}
+	
+	public static function multiplier($a, $b)
+	{
+		return ($a*$b);
+	}
+	
+	public static function diviser($a, $b)
+	{
+		return ($a/$b);
+	}
+
+}
